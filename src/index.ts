3f3839d22c0aea6da6db483adb5050f27225c150
Lucasgfv <lucascomput23@gmail.com>
2026-05-03 16:02:08 -0300

    chore: add fastify swagger generation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config'
 import Fastify, { fastify } from 'fastify'
-
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
 import z from 'zod';
+
 
 const app = Fastify({
   logger: true
@@ -10,6 +12,25 @@ const app = Fastify({
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'BootCamp',
+      description: 'API do BootCamp',
+      version: '1.0.0',
+    },
+    servers: [{
+      description: "LocalHost",
+      url: "http://localhost:8081"
+    }],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+});
 
 app.withTypeProvider<ZodTypeProvider>().route({
     method: 'GET',
